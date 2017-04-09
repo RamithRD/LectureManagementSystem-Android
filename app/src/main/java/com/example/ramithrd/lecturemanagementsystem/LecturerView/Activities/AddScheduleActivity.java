a6df1c6401d353363f81b353634412dea384b7dd
RamithRD <ramithrandeniya@gmail.com>
2017-04-09 23:26:10 +0530

    Lecture date picker dialog added.

--- app/src/main/java/com/example/ramithrd/lecturemanagementsystem/LecturerView/Activities/AddScheduleActivity.java
+++ app/src/main/java/com/example/ramithrd/lecturemanagementsystem/LecturerView/Activities/AddScheduleActivity.java
@@ -1,5 +1,6 @@
 package com.example.ramithrd.lecturemanagementsystem.LecturerView.Activities;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -21,8 +23,14 @@ import java.util.Calendar;
 public class AddScheduleActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     private ImageButton pickLecTime;
+    private ImageButton pickLecDate;
+
     private EditText selectedStartTimeTxt;
     private EditText selectedEndTimeTxt;
+    private EditText selectedDateTxt;
+
+    private int mYear, mMonth, mDay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,7 @@ public class AddScheduleActivity extends AppCompatActivity implements TimePicker
 
         selectedStartTimeTxt = (EditText) findViewById(R.id.schedule_picked_layout);
         selectedEndTimeTxt = (EditText) findViewById(R.id.schedule_end_time);
+        selectedDateTxt = (EditText) findViewById(R.id.schedule_picked_date);
 
         pickLecTime = (ImageButton) findViewById(R.id.lecTimeBtn);
         pickLecTime.setOnClickListener(new View.OnClickListener() {
@@ -38,32 +47,58 @@ public class AddScheduleActivity extends AppCompatActivity implements TimePicker
             public void onClick(View v) {
 
                 Calendar now = Calendar.getInstance();
-                TimePickerDialog tpd = TimePickerDialog.newInstance(
+                TimePickerDialog timePicker = TimePickerDialog.newInstance(
                         AddScheduleActivity.this,
                         now.get(Calendar.HOUR_OF_DAY),
                         now.get(Calendar.MINUTE),
                         false
                 );
-                tpd.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                timePicker.setOnCancelListener(new DialogInterface.OnCancelListener() {
                     @Override
                     public void onCancel(DialogInterface dialogInterface) {
                         Log.d("TimePicker", "Dialog was cancelled");
                     }
                 });
-                tpd.show(getFragmentManager(), "Timepickerdialog");
+
+                timePicker.show(getFragmentManager(), "BTimepickerdialog");
+
+            }
+        });
+
+        pickLecDate = (ImageButton) findViewById(R.id.lecDateBtn);
+        pickLecDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Calendar calendar = Calendar.getInstance();
+                mYear = calendar.get(Calendar.YEAR);
+                mMonth = calendar.get(Calendar.MONTH);
+                mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePicker = new DatePickerDialog(AddScheduleActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                        selectedDateTxt.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+
+                //sets today's date as minimum date and all the past dates are disabled
+                datePicker.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+                datePicker.setCancelable(false);
+                datePicker.show();
 
             }
         });
     }
-
-
-
 
     @Override
     public void onResume() {
         super.onResume();
 
     }
+
 
     @Override
     public void onTimeSet(RadialPickerLayout view, int hourOfDay, int minute, int hourOfDayEnd, int minuteEnd) {
@@ -75,8 +110,8 @@ public class AddScheduleActivity extends AppCompatActivity implements TimePicker
 
         String startTime = hourString+"."+minuteString;
         String endTime = hourStringEnd+"."+minuteStringEnd;
+
         selectedStartTimeTxt.setText(startTime);
         selectedEndTimeTxt.setText(endTime);
-
     }
 }
